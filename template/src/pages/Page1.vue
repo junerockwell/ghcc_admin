666d9135bdb3639c52f93539edc3178fed082a35
June Rockwell <cherubeflugel@gmail.com>
2017-09-27 12:02:46 -0700

    Create Page1.vue
    Same Page component.

---
+++ template/src/pages/Page1.vue
@@ -0,0 +1,28 @@
+<!--
+Please refer to projects such as wan_admin* and ntn_admin* on how to 
+create a Vue Page Component.
+-->
+<template>
+<div>
+<h1>Page 1</h1>
+<!-- <p>{{ count }}</p> -->
+<!-- <button @click="increment()">inc</button>
+<button @click="localMethod()">local</button> -->
+</div>
+</template>
+
+<script>
+// import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  // computed: {
+  //   ...mapGetters(['count'])
+  // },
+  // methods: {
+  //   ...mapActions(['increment']),
+  //   localMethod() {
+  //     console.log("localMethod()");
+  //   }
+  // }
+}
+</script>
