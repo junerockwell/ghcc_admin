bef0bc465e6b7ba784e843feac4f9a7825fdc2fe
June Rockwell <cherubeflugel@gmail.com>
2017-09-27 10:34:34 -0700

    Create StaticProgressBar.vue
    The Static Progress Bar based heavily on Bootstrap UI that's used by all GHCC vue admin projects.

---
+++ template/src/components/StaticProgressBar.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="progress">
+  <div
+   class="progress-bar"
+   :class="{'progress-bar-striped': striped, 'active': active}"
+   role="progressbar"
+   aria-valuenow="100"
+   aria-valuemin="100"
+   aria-valuemax="100" style="width:100%">
+    <!-- <span class="sr-only">70% Complete</span> -->
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    striped: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
