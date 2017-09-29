8f2ddccf301a49ecce3f61fc8f57026e07ebe6ac
June Rockwell <cherubeflugel@gmail.com>
2017-09-29 07:53:59 -0700

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -4,9 +4,6 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{#router}}
-import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-{{/router}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
